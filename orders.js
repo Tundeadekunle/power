9dbfc52fca6b19f8e510193cf780bf9470abb94a
Tunde adekunle <tundeadekunleft@gmail.com>
2023-04-01 14:55:46 +0100

    First Commit

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: 'POWER 10KVA Transformer',
+    productNumber: '01631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'POWER Light Bulb',
+    productNumber: '18189',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+  },
+  {
+    productName: 'POWER Electric Cable',
+    productNumber: '12211',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'POWER SOlar Panel',
+    productNumber: '96341',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'POWER Electric Pole',
+    productNumber: '99821',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'POWER Electic Engine',
+    productNumber: '71975',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'POWER Electric Cable',
+    productNumber: '11631',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  }
+]
